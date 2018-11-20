82470b6ac10ecf1f0af0050c5019391b6d4a9d96
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2018-11-20 11:35:39 +0300

    Created a var imageURL and made a prepare to fetch image

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
     
+    var imageURL: URL? {
+        didSet {
+            imageView.image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private func fetchImage() {
+        
+    }
+    
 }
