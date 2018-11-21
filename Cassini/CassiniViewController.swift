29cc5e1a99be5381cb40eb369a62c3bc43c380bc
Artyom Sadyrin <asadyryn@icloud.com>
2018-11-21 13:07:43 +0300

    Added title to detail VC on iPad

--- Cassini/CassiniViewController.swift
+++ Cassini/CassiniViewController.swift
@@ -44,7 +44,11 @@ class CassiniViewController: UIViewController, UISplitViewControllerDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             if let url = NASA[identifier] {
-                if let imageVC = segue.destination as? ImageViewController {
+                var destination = segue.destination
+                if let navcon = destination as? UINavigationController {
+                    destination = navcon.visibleViewController ?? navcon
+                }
+                if let imageVC = destination as? ImageViewController {
                     imageVC.imageURL = url
                     imageVC.title = (sender as? UIButton)?.currentTitle
                 }
